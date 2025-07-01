2bd0d1a6cd1b8a999bfab4e345be7efe683c69cd
Davifreitas77 <davifreitaz999@gmail.com>
2025-07-01 18:00:47 -0300

    adicionando projeto

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,69 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import { Stack } from "expo-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import '../global.css'
+import { TouchableOpacity, View, Text } from "react-native";
+import Ionicons from '@expo/vector-icons/Ionicons';
+import Fontisto from '@expo/vector-icons/Fontisto';
+import { ThemeProvider, useTheme } from "../src/ThemeContext";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const queryClient = new QueryClient();
+function CustomHeader() {
+  const { theme, toggleTheme } = useTheme();
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
+  var hours = new Date().getHours(); //To get the Current Hours
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        height: 40,
+        backgroundColor: theme === 'dark' ? '#16181b' : '#dfdfdf',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 16,
+      }}
+    >
+      {hours > 5 && hours < 12 ? (
+        <Text className={`text-xl font-medium ${theme === 'dark' ? 'text-white' : 'text-black'}`}>Bom dia</Text>
+      ) : hours > 12 && hours < 18 ? (
+        <Text className={`text-xl font-medium ${theme === 'dark' ? 'text-white' : 'text-black'}`}>Boa Tarde</Text>
+      ) : (
+        <Text className={`text-xl font-medium ${theme === 'dark' ? 'text-white' : 'text-black'}`}>Boa Noite</Text>
+      )}
+      <TouchableOpacity onPress={toggleTheme}>
+        <Ionicons
+          name={theme === 'dark' ? 'sunny' : 'moon'}
+          size={24}
+          color={theme === 'dark' ? 'orange' : 'black'}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+function Pages() {
+  
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          header: () => <CustomHeader />,
+
+        }}
+      />
+   
+
+    </Stack>
+  );
+}
+
+export default function Layout() {
+  return (
+    <ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <Pages />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
